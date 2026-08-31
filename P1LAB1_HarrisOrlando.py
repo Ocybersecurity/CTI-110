d58b05d4e6f0e87428ff38231583ca0c4cb842cc
Ocybersecurity <harriso7354@student.faytechcc.edu>
2026-08-31 06:31:42 -0400

    First true Python assignment
    Taking input from the user and printing output

---
+++ P1LAB1_HarrisOrlando.py
@@ -0,0 +1,8 @@
+# Orlando Harris
+# P1LAB1.PY
+# input and print
+
+first_name = input("What is your firstname:  ")
+last_name = input("What is your lastname:  ")
+
+print("Hello,", first_name, last_name + "!","Welcome to CTI-110!")
